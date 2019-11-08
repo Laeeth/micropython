1c423bb42d857e4fbc0529c7d224754ec4de6650
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-11-08 02:45:05 +0200

    py/obj: Reuse mp_obj_get_int_maybe() in mp_obj_get_int().
    The maybe() variant was added in 8270e3853dc167d, and get_int() was already there.
    I didn't see any mention as to why it was cloned, so I guess it went unnoticed, or
    that I missed something.
    
    mp_obj_get_int() is used extensively so it's debatable whether this change, which
    incurs a tiny code decrease, is worth the slight performance hit.

--- py/obj.c
+++ py/obj.c
@@ -226,18 +226,14 @@ mp_int_t mp_obj_get_int(mp_const_obj_t arg) {
     // This function essentially performs implicit type conversion to int
     // Note that Python does NOT provide implicit type conversion from
     // float to int in the core expression language, try some_list[1.0].
-    if (arg == mp_const_false) {
-        return 0;
-    } else if (arg == mp_const_true) {
-        return 1;
-    } else if (mp_obj_is_small_int(arg)) {
-        return MP_OBJ_SMALL_INT_VALUE(arg);
-    } else if (mp_obj_is_type(arg, &mp_type_int)) {
-        return mp_obj_int_get_checked(arg);
-    } else {
-        mp_obj_t res = mp_unary_op(MP_UNARY_OP_INT, (mp_obj_t)arg);
-        return mp_obj_int_get_checked(res);
+
+    mp_int_t n;
+    if (mp_obj_get_int_maybe(arg, &n)) {
+        return n;
     }
+
+    mp_obj_t res = mp_unary_op(MP_UNARY_OP_INT, (mp_obj_t)arg);
+    return mp_obj_int_get_checked(res);
 }
 
 mp_int_t mp_obj_get_int_truncated(mp_const_obj_t arg) {
